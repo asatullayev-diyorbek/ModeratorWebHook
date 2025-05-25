fda15cd48d202c2c8478bcfd02f73c4b3423a789
asatullayev-diyorbek <asatullayevdiyorbek3@gmail.com>
2025-05-25 15:58:26 +0500

    Fixed admin.py

--- config/settings.py
+++ config/settings.py
@@ -204,3 +204,24 @@ UNFOLD = {
 
     }
 }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/home/ubuntu/ModeratorWebHook/logs/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
